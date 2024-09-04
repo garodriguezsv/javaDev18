e8bfb67dbda09a367b5edefd138e8a8d5926fd00
Gerson Rodriguez <50417798+garodriguezsv@users.noreply.github.com>
2024-09-03 20:27:00 -0600

    Agregando otra linea de mensaje

--- src/Incrementos.java
+++ src/Incrementos.java
@@ -5,5 +5,7 @@ public class Incrementos {
         System.out.println("Firs value: " + ++x);
         System.out.println("Second value: " + x--);
         System.out.println("Third value: " + x);
+
+        System.out.println("Texto agregado de ultimo");
     }
 }
